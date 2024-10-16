7e9baf8b3e0b1b1df23df321a3519f90894fcbe5
admin <admin@admins-MBP.home>
2024-10-16 08:51:35 -0700

    updates

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,44 +1,52 @@
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
 
 function reducer(state, action) {
   console.log(state, action);
-  if (action.type === "inc") return state + 1;
-  if (action.type === "dec") return state - 1;
-  if (action.type === "setCount") return action.payloads;
+
+  switch (action.type) {
+    case "dec":
+      return { ...state, count: state.count - state.step };
+    case "inc":
+      return { ...state, count: state.count + state.step };
+    case "setCount":
+      return { ...state, count: action.payloads };
+    case "setStep":
+      return { ...state, step: action.payloads };
+    case "reset":
+      return { ...state, count: 0 };
+    default:
+      throw new Error("Unknown action");
+  }
 }
 
 function DateCounter() {
-  const [count, dispatch] = useReducer(reducer, 0);
-  const [step, setStep] = useState(1);
+  //How to use useReducer Hooks
+  const initialState = { count: 0, step: 1 };
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    // setCount((count) => count - 1);
-    //setCount((count) => count - step);
     dispatch({ type: "dec" });
   };
 
   const inc = function () {
-    // setCount((count) => count + 1);
-    //setCount((count) => count + step);
     dispatch({ type: "inc" });
   };
 
   const defineCount = function (e) {
-    //setCount(Number(e.target.value));
     dispatch({ type: "setCount", payloads: Number(e.target.value) });
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    dispatch({ type: "setStep", payloads: Number(e.target.value) });
   };
 
   const reset = function () {
-    //setCount(0);
-    setStep(1);
+    dispatch({ type: "reset" });
   };
 
   return (
